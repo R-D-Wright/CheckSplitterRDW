b371d13e61daaa5c5289a982910b366bb940a7b0
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-04-11 09:19:49 -0500

    Initial Commit

---
+++ CheckSplitter/CheckSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSplitterApp.swift
+//  CheckSplitter
+//
+//  Created by Matthew RIley on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CheckSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
