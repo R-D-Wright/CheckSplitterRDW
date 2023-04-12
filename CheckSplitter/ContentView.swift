0637306161d01c88725ac148db5a7af6ebcd61dd
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-04-12 08:46:37 -0500

    Completed layout. Setup Text Field for check total and Picker for Number of People.

--- CheckSplitter/ContentView.swift
+++ CheckSplitter/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Specifying the value is a Double (AKA A Decimal value)
+    @State var checkAmount: Double = 0
+    
+    @State var numOfPeople = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationView {
+            
+            Form {
+                
+                Section {
+                    
+                    // Specifying the format to be the USD currency code so it will display in US Dollars
+                    TextField("$0.00", value: $checkAmount, format: .currency(code: "USD"))
+                    
+                    Picker("Number of People", selection: $numOfPeople){
+                        
+                        // Setting the range of options from 0 to 10 and creating a Text element/option for each value
+                        ForEach(0..<11){ num in
+                            
+                            // Using String() to convert the num INT value to a String so it can be displayed in the Text element
+                            Text(String(num))
+                        }
+                    }
+                }
+                
+                Section(header: Text("Select Tip Percentage")) {
+                    Text("0% 10% 15% 20%")
+                }
+                
+                Button {
+                    print("Hello World")
+                } label: {
+                    Text("CLICK TO SPLIT CHECK")
+                }
+
+                Section(header: Text("Each person pays...")) {
+                    Text("$0.00")
+                }
+            }
+            .navigationTitle("Check Splitter")
         }
-        .padding()
     }
 }
 
